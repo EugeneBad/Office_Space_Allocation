86f75f49cbe1b3a52902db2724055c151b271d19
eugenius <eudriku@gmail.com>
2017-04-25 00:08:53 +0300

    Commenting, docopt commands defined.
    Adding comments, and housekeeping.

--- room_allocator.py
+++ room_allocator.py
@@ -41,25 +41,37 @@ def docopt_cmd(func):
     fn.__dict__.update(func.__dict__)
     return fn
 
-andela_dojo = Dojo()
+
+andela_dojo = Dojo()  # Create the Dojo object.
+
 
 class InteractiveRoomAllocator(cmd.Cmd):
     intro = "Eugene's random room allocator for Andela"
     prompt = "Room_Allocator: "
     file = None
 
+    # Function to implement the CLI command create_room.
     @docopt_cmd
     def do_create_room(self, arg):
         """Usage: create_room <room_type> <room_name>"""
 
+        #  Check if room is an office.
         if arg['<room_type>'] == 'office':
+
+            #  Add it as a value to the office_spaces dictionary in andela_dojo.
             andela_dojo['office_spaces'][arg['<room_name>']] = Office(arg['<room_name>'])
+
             print('An office called {} has been successfully created!'.format(arg['<room_name>']))
 
+        #  Check if room is a living space.
         if arg['<room_type>'] == 'living':
+
+            #  Add it as a value to the living_spaces dictionary in andela_dojo.
             andela_dojo['living_spaces'][arg['<room_name>']] = Room('living', arg['<room_name>'])
+
             print('An living space called {} has been successfully created!'.format(arg['<room_name>']))
 
+    # Function to implement the CLI command add_person.
     @docopt_cmd
     def do_add_person(self, arg):
         """Usage:
@@ -67,40 +79,51 @@ class InteractiveRoomAllocator(cmd.Cmd):
         Options:
         wants_accommodation=Y/N  Yes(Y) if Fellow opts for accommodation, No(N) otherwise. [default: N]"""
 
+        # Check if person is staff and wants accommodation.
         if arg['<FELLOW|STAFF>'] == 'Staff' and arg['wants_accommodation'] == 'Y':
             print('Staff cannot be allocated living spaces')
 
+        # Randomly select an office from the office_spaces dictionary in andela_dojo,
+        # store it in random_office variable
         office_list = [office for office in andela_dojo['office_spaces'].values()]
         random_office_index = random.randrange(len(office_list)-1)
         random_office = office_list[random_office_index]
 
+        # Randomly select a living space from the living_spaces dictionary in andela_dojo,
+        # store it in random_living_space variable
         living_space_list = [living_space for living_space in andela_dojo['living_spaces'].values()]
         random_living_space_index = random.randrange(len(living_space_list)-1)
         random_living_space = office_list[random_living_space_index]
 
+        # If staff entry is valid, add person to Staff dictionary in the occupants attribute of the random_office
         if arg['<FELLOW|STAFF>'] == 'Staff' and arg['wants_accommodation'] != 'Y':
             random_office.occupants['Staff'][arg['<person_name>']] = Person(arg['<person_name>'], 'Staff')
 
             print('Staff {} has been added successfully!'.format(arg['<person_name>']))
             print('{} has been given office: {}'.format(arg['<person_name>'], random_office.name))
 
+        # If fellow wants accommodation:
         if arg['<FELLOW|STAFF>'] == 'Fellow' and arg['wants_accommodation'] == 'Y':
 
+            # Add Fellow to Fellow dictionary in the occupants attribute of the random_office
             random_office.occupants['Fellow'][arg['<person_name>']] = Fellow(arg['<person_name>'], 'Staff', 'Y')
+
+            # Add Fellow to Fellow dictionary in the occupants attribute of the random_random_living_space
             random_living_space.occupants[arg['<person_name>']] = Fellow(arg['<person_name>'], 'Staff', 'Y')
 
             print('Fellow {} has been added successfully!'.format(arg['<person_name>']))
             print('{} has been given office: {}'.format(arg['<person_name>'], random_office.name))
             print('{} has been given living space: {}'.format(arg['<person_name>'], random_living_space.name))
 
+        # If fellow does not want accommodation:
         if arg['<FELLOW|STAFF>'] == 'Fellow' and arg['wants_accommodation'] != 'Y':
+
+            # Add Fellow to Fellow dictionary in the occupants attribute of the random_office
             random_office.occupants['Fellow'][arg['<person_name>']] = Fellow(arg['<person_name>'], 'Staff', 'Y')
 
             print('Fellow {} has been added successfully!'.format(arg['<person_name>']))
             print('{} has been given office: {}'.format(arg['<person_name>'], random_office.name))
 
 
-
 opt = docopt(__doc__, sys.argv[1:])
 InteractiveRoomAllocator().cmdloop()
-{'k':9}.__len__()
