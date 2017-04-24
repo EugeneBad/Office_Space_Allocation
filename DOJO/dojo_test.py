862457fbee9c8ad7d88884f29a066ef82a19d67a
eugenius <eudriku@gmail.com>
2017-04-25 00:26:53 +0300

    Second Dojo class test. Dojo should create, dictionary objects.

--- DOJO/dojo_test.py
+++ DOJO/dojo_test.py
@@ -4,7 +4,10 @@ from .dojo import Dojo
 
 class DojoTest(unittest.TestCase):
     def setUp(self):
-        some_dojo = Dojo
+        self.some_dojo = Dojo()
 
     def test_dojo_inherits_from_dict(self):
         self.assertTrue(issubclass(Dojo, dict), msg='The Dojo class should inherit from the dict class')
+
+    def test_dojo_object_is_a_dictionary(self):
+        self.assertEqual(type(self.some_dojo), dict, msg='The Dojo class is not creating dictionary objects')
