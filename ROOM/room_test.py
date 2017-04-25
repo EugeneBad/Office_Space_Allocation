052229b600568499aa10f050666f867bcae9ab90
eugenius <eudriku@gmail.com>
2017-04-25 10:11:32 +0300

    Added Room class tests. Test for room name and type, test that room occupants is a dictionary

---
+++ ROOM/room_test.py
@@ -0,0 +1,21 @@
+from ROOM.room import Room
+import unittest
+
+
+class RoomTest(unittest.TestCase):
+
+    def setUp(self):
+        self.office = Room('office', 'Grey')
+        self.living = Room('living', 'White')
+
+    def test_room_name(self):
+        self.assertEqual(self.office.name, 'Grey', msg='Invalid office name')
+        self.assertEqual(self.living.name, 'White', msg='Invalid living space name')
+
+    def test_room_type(self):
+        self.assertEqual(self.office.type, 'office', msg='Office space type should office')
+        self.assertEqual(self.living.type, 'living', msg='Living space type should living')
+
+    def test_room_occupants_is_dictionary(self):
+        self.assertEqual(type(self.office.occupants), dict, msg='Office space occupants is not a dictionary')
+        self.assertEqual(type(self.living.occupants), dict, msg='Living space occupants is not a dictionary')
