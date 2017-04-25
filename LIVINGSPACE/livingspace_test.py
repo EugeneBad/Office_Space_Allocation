8632f44a5099d5e9b446236b0c41ce7dd0122fef
eugenius <eudriku@gmail.com>
2017-04-25 10:54:55 +0300

    Renamed office tests and used them as livingspace tests

---
+++ LIVINGSPACE/livingspace_test.py
@@ -0,0 +1,27 @@
+import unittest
+from LIVINGSPACE.livingspace import LivingSpace
+from ROOM.room import Room
+
+
+class LivingSpaceTest(unittest.TestCase):
+
+    def setUp(self):
+        self.some_living_space = LivingSpace('Yellow')
+
+    def test_LivingSpace_inherits_Room(self):
+        self.assertTrue(issubclass(LivingSpace, Room), msg='LivingSpace is not inheriting from Room')
+
+    def test_living_space_name(self):
+        self.assertEqual(self.some_living_space.name, 'Yellow', msg='Invalid living space name')
+
+    def test_living_space_type(self):
+        self.assertEqual(self.some_living_space.type, 'living', msg='Living space type should be living')
+
+    def test_living_space_occupants_is_dictionary(self):
+        self.assertEqual(type(self.some_living_space.occupants), dict, msg='Living space occupants is not a dictionary')
+
+    def test_maximum_living_sapce_occupants(self):
+        self.assertEqual(self.some_living_space.maximum_occupants, 6, msg='Wrong number of maximum living space occupants')
+
+    def test_living_space_occupants(self):
+        self.assertEqual(type(self.some_living_space.occupants), dict, msg='No occupants dictionary in LivingSpace class ')
